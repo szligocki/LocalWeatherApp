03bc301fa56e819552278d8f5df12f388ac54613
Szymon Ligocki <szymon.ligocki@coig.pl>
2018-07-09 12:51:34 +0200

    Added comments

--- app/src/main/java/sz/pl/localweatherapp/service/OpenWeatherMapService.java
+++ app/src/main/java/sz/pl/localweatherapp/service/OpenWeatherMapService.java
@@ -8,6 +8,15 @@ import sz.pl.localweatherapp.dto.city.WeatherMapDto;
 
 public interface OpenWeatherMapService {
 
+    /**
+     * Generates a URL for OpenWeatherMap
+     *
+     * @param cityId - city id
+     * @param lang   - language of returned data
+     * @param units  - Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+     * @param appid  - API KEY
+     * @return WeatherMapDto
+     */
     @GET("weather")
     Call<WeatherMapDto> getWeatherByCityId(
             @Query("id") long cityId,
